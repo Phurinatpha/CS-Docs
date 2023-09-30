1b01884ea77d5dac3da3dd3bb86627ca6a5c6010
Leoscope <aum6461@gmail.com>
2023-09-30 21:32:34 +0700

    fix some know bug about pdf

--- web/app/static/js/form.js
+++ web/app/static/js/form.js
@@ -121,7 +121,12 @@ $(document).ready(function () {
     // Append the file to the FormData object
     var file = $('input[name="doc_data"]')[0].files[0];
     formData.append('doc_data', file);
-
+    if (document.getElementById('drag-and-drop').style.display != 'none'){
+      formData.append('delete_pdf', true); 
+    }
+    else{
+      formData.append('delete_pdf', false);
+    }
     // Add other form data to the FormData object
     formData.append('id', $('#doc_id').val());
     formData.append('subject', $('#descrip').val());
@@ -142,6 +147,10 @@ $(document).ready(function () {
       contentType: false,
       processData: false,
       success: function () {
+        $('#modal-form').modal('toggle');
+        resetForm();
+        refresh();
+        get_countNumber();
         Swal.fire({
           position: 'center',
           icon: 'success',
@@ -149,10 +158,6 @@ $(document).ready(function () {
           showConfirmButton: false,
           timer: 1500
         })
-        $('#modal-form').modal('toggle');
-        resetForm();
-        refresh();
-        get_countNumber();
 
       },
       error: function (error) {
