392fd53246e60d15fd48a0ca9c1ff76171f49c01
LunaTearR <saengtawanpoopoom@gmail.com>
2023-10-02 12:28:54 +0700

    fix-bug-sweetalert

--- web/app/static/js/base.js
+++ web/app/static/js/base.js
@@ -25,7 +25,7 @@ function toggleNavbar() {
 
 // Set the session timeout duration in milliseconds (e.g., 1 hour)
 const sessionTimeoutDuration = 3600000; // 1 hr in milliseconds
-// const sessionTimeoutDuration = 5000; // 1 hr in milliseconds
+// const sessionTimeoutDuration = 10000; // 1 hr in milliseconds
 
 
 // Function to log the user out
